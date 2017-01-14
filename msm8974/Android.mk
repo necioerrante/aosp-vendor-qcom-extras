34491b81aa2d09600474a3eaa59f5036602ab08c
necioerrante <necioerrante@gmail.com>
2017-01-14 15:41:31 +0300

    Update from https://github.com/rcstar6696/vendor-sony-audio

--- msm8974/Android.mk
+++ msm8974/Android.mk
@@ -7,66 +7,274 @@ ifneq ($(filter rhine shinano, $(PRODUCT_PLATFORM)), )
 # ---------------------------------------------------------------------------------
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := Bluetooth_cal
+LOCAL_MODULE            := MTP_Bluetooth_cal.acdb
+LOCAL_MODULE_FILENAME   := MTP_Bluetooth_cal.acdb
 LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_SUFFIX     := .acdb
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/MTP/
-LOCAL_SRC_FILES         := etc/acdbdata/MTP/Bluetooth_cal.acdb
+LOCAL_SRC_FILES         := etc/acdbdata/MTP/MTP_Bluetooth_cal.acdb
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := General_cal
+LOCAL_MODULE            := MTP_General_cal.acdb
+LOCAL_MODULE_FILENAME   := MTP_General_cal.acdb
 LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_SUFFIX     := .acdb
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/MTP/
-LOCAL_SRC_FILES         := etc/acdbdata/MTP/General_cal.acdb
+LOCAL_SRC_FILES         := etc/acdbdata/MTP/MTP_General_cal.acdb
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := Global_cal
+LOCAL_MODULE            := MTP_Global_cal.acdb
+LOCAL_MODULE_FILENAME   := MTP_Global_cal.acdb
 LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_SUFFIX     := .acdb
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/MTP/
-LOCAL_SRC_FILES         := etc/acdbdata/MTP/Global_cal.acdb
+LOCAL_SRC_FILES         := etc/acdbdata/MTP/MTP_Global_cal.acdb
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := Handset_cal
+LOCAL_MODULE            := MTP_Handset_cal.acdb
+LOCAL_MODULE_FILENAME   := MTP_Handset_cal.acdb
 LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_SUFFIX     := .acdb
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/MTP/
-LOCAL_SRC_FILES         := etc/acdbdata/MTP/Handset_cal.acdb
+LOCAL_SRC_FILES         := etc/acdbdata/MTP/MTP_Handset_cal.acdb
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := Hdmi_cal
+LOCAL_MODULE            := MTP_Hdmi_cal.acdb
+LOCAL_MODULE_FILENAME   := MTP_Hdmi_cal.acdb
 LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_SUFFIX     := .acdb
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/MTP/
-LOCAL_SRC_FILES         := etc/acdbdata/MTP/Hdmi_cal.acdb
+LOCAL_SRC_FILES         := etc/acdbdata/MTP/MTP_Hdmi_cal.acdb
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := Headset_cal
+LOCAL_MODULE            := MTP_Headset_cal.acdb
+LOCAL_MODULE_FILENAME   := MTP_Headset_cal.acdb
 LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_SUFFIX     := .acdb
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/MTP/
-LOCAL_SRC_FILES         := etc/acdbdata/MTP/Headset_cal.acdb
+LOCAL_SRC_FILES         := etc/acdbdata/MTP/MTP_Headset_cal.acdb
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := Speaker_cal
+LOCAL_MODULE            := MTP_MTP_Speaker_cal.acdb
+LOCAL_MODULE_FILENAME   := Speaker_cal.acdb
 LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_SUFFIX     := .acdb
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/MTP/
-LOCAL_SRC_FILES         := etc/acdbdata/MTP/Speaker_cal.acdb
+LOCAL_SRC_FILES         := etc/acdbdata/MTP/MTP_Speaker_cal.acdb
 include $(BUILD_PREBUILT)
 
-endif # PRODUCT_PLATFORM
+# ---------------------------------------------------------------------------------
+#             Populate ACDB data files to file system for Liquid
+# ---------------------------------------------------------------------------------
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Liquid_Bluetooth_cal.acdb
+LOCAL_MODULE_FILENAME   := Liquid_Bluetooth_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Liquid/
+LOCAL_SRC_FILES         := etc/acdbdata/Liquid/Liquid_Bluetooth_cal.acdb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Liquid_General_cal.acdb
+LOCAL_MODULE_FILENAME   := Liquid_General_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Liquid/
+LOCAL_SRC_FILES         := etc/acdbdata/Liquid/Liquid_General_cal.acdb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Liquid_Global_cal.acdb
+LOCAL_MODULE_FILENAME   := Liquid_Global_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Liquid/
+LOCAL_SRC_FILES         := etc/acdbdata/Liquid/Liquid_Global_cal.acdb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Liquid_Handset_cal.acdb
+LOCAL_MODULE_FILENAME   := Liquid_Handset_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Liquid/
+LOCAL_SRC_FILES         := etc/acdbdata/Liquid/Liquid_Handset_cal.acdb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Liquid_Hdmi_cal.acdb
+LOCAL_MODULE_FILENAME   := Liquid_Hdmi_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Liquid/
+LOCAL_SRC_FILES         := etc/acdbdata/Liquid/Liquid_Hdmi_cal.acdb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Liquid_Headset_cal.acdb
+LOCAL_MODULE_FILENAME   := Liquid_Headset_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Liquid/
+LOCAL_SRC_FILES         := etc/acdbdata/Liquid/Liquid_Headset_cal.acdb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Liquid_Speaker_cal.acdb
+LOCAL_MODULE_FILENAME   := Liquid_Speaker_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Liquid/
+LOCAL_SRC_FILES         := etc/acdbdata/Liquid/Liquid_Speaker_cal.acdb
+include $(BUILD_PREBUILT)
+
+# ---------------------------------------------------------------------------------
+#             Populate ACDB data files to file system for Fluid
+# ---------------------------------------------------------------------------------
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Fluid_Bluetooth_cal.acdb
+LOCAL_MODULE_FILENAME   := Fluid_Bluetooth_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Fluid/
+LOCAL_SRC_FILES         := etc/acdbdata/Fluid/Fluid_Bluetooth_cal.acdb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Fluid_General_cal.acdb
+LOCAL_MODULE_FILENAME   := Fluid_General_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Fluid/
+LOCAL_SRC_FILES         := etc/acdbdata/Fluid/Fluid_General_cal.acdb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Fluid_Global_cal.acdb
+LOCAL_MODULE_FILENAME   := Fluid_Global_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Fluid/
+LOCAL_SRC_FILES         := etc/acdbdata/Fluid/Fluid_Global_cal.acdb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Fluid_Handset_cal.acdb
+LOCAL_MODULE_FILENAME   := Fluid_Handset_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Fluid/
+LOCAL_SRC_FILES         := etc/acdbdata/Fluid/Fluid_Handset_cal.acdb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Fluid_Hdmi_cal.acdb
+LOCAL_MODULE_FILENAME   := Fluid_Hdmi_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Fluid/
+LOCAL_SRC_FILES         := etc/acdbdata/Fluid/Fluid_Hdmi_cal.acdb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Fluid_Headset_cal.acdb
+LOCAL_MODULE_FILENAME   := Fluid_Headset_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Fluid/
+LOCAL_SRC_FILES         := etc/acdbdata/Fluid/Fluid_Headset_cal.acdb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := Fluid_Speaker_cal.acdb
+LOCAL_MODULE_FILENAME   := Speaker_cal.acdb
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/acdbdata/Fluid/
+LOCAL_SRC_FILES         := etc/acdbdata/Fluid/Fluid_Speaker_cal.acdb
+include $(BUILD_PREBUILT)
+
+# ------------------------------------------------------------------------------
+# 32BIT LIBS
+# ------------------------------------------------------------------------------
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := libacdb-fts
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_SUFFIX     := .so
+LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib
+LOCAL_MULTILIB          := 32
+LOCAL_SRC_FILES         := vendor/lib/libacdb-fts.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := libacdbloader
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_SUFFIX     := .so
+LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib
+LOCAL_MULTILIB          := 32
+LOCAL_SRC_FILES         := vendor/lib/libacdbloader.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := libacdbmapper
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_SUFFIX     := .so
+LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib
+LOCAL_MULTILIB          := 32
+LOCAL_SRC_FILES         := vendor/lib/libacdbmapper.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := libacdbrtac
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_SUFFIX     := .so
+LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib
+LOCAL_MULTILIB          := 32
+LOCAL_SRC_FILES         := vendor/lib/libacdbrtac.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := libadiertac
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_SUFFIX     := .so
+LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib
+LOCAL_MULTILIB          := 32
+LOCAL_SRC_FILES         := vendor/lib/libadiertac.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := libaudcal
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_SUFFIX     := .so
+LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib
+LOCAL_MULTILIB          := 32
+LOCAL_SRC_FILES         := vendor/lib/libaudcal.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := libaudioalsa
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_SUFFIX     := .so
+LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib
+LOCAL_MULTILIB          := 32
+LOCAL_SRC_FILES         := vendor/lib/libaudioalsa.so
+include $(BUILD_PREBUILT)
+
+endif
--- proprietary.mk
+++ proprietary.mk
@@ -9,10 +9,24 @@ PRODUCT_PACKAGES += \
     libaudcal
 
 PRODUCT_PACKAGES += \
-    Bluetooth_cal \
-    General_cal \
-    Global_cal \
-    Handset_cal \
-    Hdmi_cal \
-    Headset_cal \
-    Speaker_cal
+    MTP_Bluetooth_cal.acdb \
+    MTP_General_cal.acdb \
+    MTP_Global_cal.acdb \
+    MTP_Handset_cal.acdb \
+    MTP_Hdmi_cal.acdb \
+    MTP_Headset_cal.acdb \
+    MTP_Speaker_cal.acdb \
+    Liquid_Bluetooth_cal.acdb \
+    Liquid_General_cal.acdb \
+    Liquid_Global_cal.acdb \
+    Liquid_Handset_cal.acdb \
+    Liquid_Hdmi_cal.acdb \
+    Liquid_Headset_cal.acdb \
+    Liquid_Speaker_cal.acdb \
+    Fluid_Bluetooth_cal.acdb \
+    Fluid_General_cal.acdb \
+    Fluid_Global_cal.acdb \
+    Fluid_Handset_cal.acdb \
+    Fluid_Hdmi_cal.acdb \
+    Fluid_Headset_cal.acdb \
+    Fluid_Speaker_cal.acdb
